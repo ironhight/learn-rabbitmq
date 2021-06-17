7418cac3509e7f4a6e547a4f650cb255cab37eec
ironhight <mrnguyenhoainam2468@gmail.com>
2021-06-17 04:22:28 +0000

    routing

---
+++ routing/emit_log_direct.js
@@ -0,0 +1,34 @@
+const amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', function(error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+    console.log('Start Message Queue');
+
+    connection.createChannel(function(error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+
+        const exchange = 'direct_logs';
+        const args = process.argv.slice(2); // process.argv là những gì nhập sau khi chạy server
+        const msg = args.slice(1).join(' ') || 'Hello World!';
+        const severity = (args.length > 0) ? args[0] : 'info';
+
+        channel.assertExchange(exchange, 'direct', { // filter message cho mỗi consumer nhận tương ứng, dùng direct
+            durable: false
+        });
+
+        channel.publish(exchange, severity, Buffer.from(msg)); 
+
+        console.log(" [x] Sent %s: '%s'", severity, msg);
+    });
+
+    setTimeout(function() {
+        connection.close();
+        console.log('Close Message Queue');
+
+        process.exit(0);
+    }, 500);
+});
